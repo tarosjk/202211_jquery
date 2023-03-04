d2fe5c165b94897b1b98113c3066235174e44db0
tarosjk <tarosjk@gmail.com>
2023-03-04 16:13:25 +0900

    slick設定変更

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  // slick
+  $('#slick').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1000,
+    // fade: true,
+    dots: true,
+    slidesToShow: 2,
+    responsive: [
+      // ~768
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          fade: true,
+        },
+      }
+    ],
+  })
+
+});
